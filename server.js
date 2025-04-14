81d72f2e594ad55bb0b368bf83a89d2dc232c992
dhanushramudri <ramudridhanush@gmail.com>
2025-04-14 13:29:33 +0530

    updated cors

--- server.js
+++ server.js
@@ -22,12 +22,25 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
   "mongodb+srv://dhanush:dhanush@cluster0.cs8ottv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// CORS Options to allow credentials and specific origin
+// CORS Options to allow credentials and specific origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://events-frontend-chi.vercel.app",
+];
+
 const corsOptions = {
-  origin: "http://localhost:5173", // Your frontend URL
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type, Authorization",
-  credentials: true, // Allow credentials (cookies, headers, etc.)
+  credentials: true,
 };
 
 // Middleware
